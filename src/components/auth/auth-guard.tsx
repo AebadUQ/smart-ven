91b43b1724f9e485e03839609435963613858d01
AebadUQ <aebadulquadir123@gmail.com>
2025-10-28 17:23:34 +0500

    fix

--- src/components/auth/auth-guard.tsx
+++ src/components/auth/auth-guard.tsx
@@ -1,3 +1,40 @@
+// 'use client';
+// import { ReactNode, useEffect } from 'react';
+// import { useRouter, usePathname } from 'next/navigation';
+// import { useSelector } from 'react-redux';
+// import { RootState } from '@/store';
+
+// interface Props {
+//   children: ReactNode;
+// }
+
+// export default function AuthGuard({ children }: Props) {
+//   const token = useSelector((state: RootState) => state.auth.token);
+//   console.log("state",token)
+//   const router = useRouter();
+//   const pathname = usePathname();
+
+//   useEffect(() => {
+//     if (token) {
+//       // if (pathname.startsWith('/auth')) {
+//         router.replace('/dashboard');
+//       // }
+//     } else {
+//       if (!pathname.startsWith('/auth')) {
+//         router.replace('/auth/signin');
+//       }
+//     }
+//   }, [token, pathname, router]);
+
+//   if (
+//     (!token && !pathname.startsWith('/auth')) ||
+//     (token && pathname.startsWith('/auth'))
+//   ) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return <>{children}</>;
+// }
 'use client';
 import { ReactNode, useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
@@ -16,9 +53,9 @@ export default function AuthGuard({ children }: Props) {
 
   useEffect(() => {
     if (token) {
-      // if (pathname.startsWith('/auth')) {
+      if (pathname.startsWith('/auth')) {
         router.replace('/dashboard');
-      // }
+      }
     } else {
       if (!pathname.startsWith('/auth')) {
         router.replace('/auth/signin');
